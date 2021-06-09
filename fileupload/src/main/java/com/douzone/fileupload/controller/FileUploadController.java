703af521fb70babf1b8b4f59fddc361d2168455d
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-09 13:01:24 +0900

    fileupload #1

---
+++ fileupload/src/main/java/com/douzone/fileupload/controller/FileUploadController.java
@@ -0,0 +1,22 @@
+package com.douzone.fileupload.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileUploadController {
+
+	@RequestMapping({"","/form"})
+	public String form() {		
+		return "/WEB-INF/views/form.jsp";
+	}
+	
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public String upload(@RequestParam("file1") MultipartFile file1) { // multipartResolver 작동
+		
+		return "/WEB-INF/views/result.jsp";
+	}
+}
