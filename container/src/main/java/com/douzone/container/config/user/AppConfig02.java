6f537adfbde042b10bac663228e18d3ff666ce75
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-11 15:27:05 +0900

    wiring #4

--- container/src/main/java/com/douzone/container/config/user/AppConfig02.java
+++ container/src/main/java/com/douzone/container/config/user/AppConfig02.java
@@ -2,12 +2,10 @@ package com.douzone.container.config.user;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 
 import com.douzone.container.user.User;
 
-
+@Configuration
 public class AppConfig02 {
 
 	@Bean
