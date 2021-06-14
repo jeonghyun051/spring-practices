9411c9dc43c0fc66f25f049e912378131a1e437e
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-14 14:55:21 +0900

    explicit wiring XML-#2

--- container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerXmlConfigTest.java
+++ container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerXmlConfigTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.douzone.container.videosystem.DVDPack;
 import com.douzone.container.videosystem.DigitalVideoDisc;
 
 @RunWith(SpringJUnit4ClassRunner.class) // 스프링에서 제공해주는 ruuner, 이제 컨테이너 안에서 테스트를 한다.
@@ -30,6 +31,42 @@ public class DVDPlayerXmlConfigTest {
 	@Qualifier("ironMan")
 	DigitalVideoDisc dvd2;
 	
+	@Autowired
+	@Qualifier("avengersInfinityWar")
+	DigitalVideoDisc dvd3;
+	
+	@Autowired
+	@Qualifier("avengersEndgame")
+	DigitalVideoDisc dvd4;
+	
+	@Autowired
+	@Qualifier("avengersAgeOfUltron")
+	DigitalVideoDisc dvd5;
+	
+	@Autowired
+	@Qualifier("captainAmerica")
+	DigitalVideoDisc dvd6;
+	
+	@Autowired
+	@Qualifier("avengersDirectorEdition")
+	DigitalVideoDisc dvd7;
+	
+	@Autowired
+	@Qualifier("avengersExpansionPack1")
+	DigitalVideoDisc dvd8;
+	
+	@Autowired
+	@Qualifier("avengersExpansionPack2")
+	DigitalVideoDisc dvd9;
+	
+	@Autowired
+	@Qualifier("avengersExpansionPack3")
+	DigitalVideoDisc dvd10;
+
+	@Autowired
+	@Qualifier("avengersTriplepack")
+	DVDPack dvdPack;
+
 	@Ignore
 	@Test
 	public void testDVD1() {
@@ -39,5 +76,50 @@ public class DVDPlayerXmlConfigTest {
 	@Test
 	public void testDVD2() {
 		assertNotNull(dvd2);
+	}
+	
+	@Test
+	public void testDVD3() {
+		assertNotNull(dvd3);
+	}
+	
+	@Test
+	public void testDVD4() {
+		assertNotNull(dvd4);
+	}
+	
+	@Test
+	public void testDVD5() {
+		assertNotNull(dvd5);
+	}	
+	
+	@Test
+	public void testDVD6() {
+		assertNotNull(dvd6);
+	}
+		
+	@Test
+	public void testDVD7() {
+		assertNotNull(dvd7);
+	}
+	
+	@Test
+	public void testDVD8() {
+		assertNotNull(dvd8);
+	}
+	
+	@Test
+	public void testDVD9() {
+		assertNotNull(dvd9);
+	}
+	
+	@Test
+	public void testDVD10() {
+		assertNotNull(dvd10);
+	}
+	
+	@Test
+	public void testDVDPack() {
+		assertNotNull(dvdPack);
 	}
 }
