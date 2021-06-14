6a3030b90402098df615f1efa0f0ea845405ad34
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-14 10:48:27 +0900

    wiring #2-2

---
+++ container/src/test/java/com/douzone/container/config/soundsystem/CDPlayerJavaConfigTest.java
@@ -0,0 +1,36 @@
+package com.douzone.container.config.soundsystem;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.douzone.container.soundsystem.CDPlayer;
+
+@RunWith(SpringJUnit4ClassRunner.class) // 스프링에서 제공해주는 ruuner, 이제 컨테이너 안에서 테스트를 한다.
+@ContextConfiguration(classes=CDPlayerConfig.class) 
+public class CDPlayerJavaConfigTest {
+
+	@Rule
+	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+	
+	@Autowired
+	private CDPlayer cdPlayer;
+	
+	@Test
+	public void testCDPlayerNotNull() {
+		assertNotNull(cdPlayer);
+	}
+	
+	@Test
+	public void testPlay() {
+		cdPlayer.play();
+		assertEquals("Playing 붕붕 by 김하온",systemOutRule.getLog().replaceAll("\r\n", "").replaceAll("\n", "")); 	
+	}
+}
