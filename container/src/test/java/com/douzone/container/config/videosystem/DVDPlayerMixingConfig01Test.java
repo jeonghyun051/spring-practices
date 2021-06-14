29b3e24b24fe9947c105a289fc3eaec17646755e
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-14 16:13:29 +0900

    explicit wiring: xml-#3

--- container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerMixingConfig01Test.java
+++ container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerMixingConfig01Test.java
@@ -8,15 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.douzone.container.config.videosystem.mixing.DVDPlayerConfig;
 import com.douzone.container.videosystem.DVDPlayer;
 
-@RunWith(SpringJUnit4ClassRunner.class) // 스프링에서 제공해주는 ruuner, 이제 컨테이너 안에서 테스트를 한다.
-@ContextConfiguration(classes=DVDPlayerConfig.class) 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=DVDPlayerConfig.class)
 public class DVDPlayerMixingConfig01Test {
-	
 	@Autowired
 	private DVDPlayer player;
-	
+
 	@Test
 	public void testPlayer() {
 		assertNotNull(player);
