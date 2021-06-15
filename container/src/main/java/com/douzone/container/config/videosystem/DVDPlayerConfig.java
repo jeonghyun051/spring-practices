ab96ceabd3b0b9c79a4d4ab54b109e9e95174e44
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-15 16:47:17 +0900

    DVDPlayerConfig

--- container/src/main/java/com/douzone/container/config/videosystem/DVDPlayerConfig.java
+++ container/src/main/java/com/douzone/container/config/videosystem/DVDPlayerConfig.java
@@ -9,34 +9,32 @@ import com.douzone.container.videosystem.DigitalVideoDisc;
 
 @Configuration
 public class DVDPlayerConfig {
- 
-	// 명시적으로 bean 설정 , sacn 으로 안함
 	@Bean
 	public Avengers avengers() {
 		return new Avengers();
 	}
 	
-	// 주입 (Injection)하기1 
-	// Bean 생성 메서드 직접 호출하기: 생성자 주입
+	// 주입(Injection)하기1:
+	// Bean 생성 메소드 직접 호출
 	// 생성자 주입
-	//@Bean
-	public DVDPlayer dvdPlayer1() {
+	// @Bean
+	public  DVDPlayer dvdPlayer01() {
 		return new DVDPlayer(avengers());
 	}
-	
-	// 주입 (Injection)하기2
-	// 파라미터로 Bean 생성 메서드 직접 호출하기: 생성자 주입
+
+	// 주입(Injection)하기2:
+	// 파라미터로 Bean 전달하기
 	// 생성자 주입
-	//@Bean
-	public DVDPlayer dvdPlayer2(DigitalVideoDisc dvd) {
+	// @Bean
+	public  DVDPlayer dvdPlayer02(DigitalVideoDisc dvd) {
 		return new DVDPlayer(dvd);
 	}
-	
-	// 주입 (Injection)하기3
+
+	// 주입(Injection)하기2:
 	// 파라미터로 Bean 전달하기
 	// setter 주입
 	@Bean
-	public DVDPlayer dvdPlayer3(DigitalVideoDisc dvd) {
+	public DVDPlayer dvdPlayer03(DigitalVideoDisc dvd) {
 		DVDPlayer player = new DVDPlayer();
 		player.setDvd(dvd);
 		return player;
@@ -44,7 +42,7 @@ public class DVDPlayerConfig {
 	
 	// 같은 타입의 빈을 생성할 경우
 	@Bean(name="player04")
-	public DVDPlayer dvdPlayer4(DigitalVideoDisc dvd) {
+	public DVDPlayer dvdPlayer04(DigitalVideoDisc dvd) {
 		DVDPlayer player = new DVDPlayer();
 		player.setDvd(dvd);
 		return player;
