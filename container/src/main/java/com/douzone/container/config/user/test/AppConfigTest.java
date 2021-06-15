4be5f278c4587439f2e567d7b501e9f523dfd4be
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-15 17:03:38 +0900

    update

--- container/src/main/java/com/douzone/container/config/user/test/AppConfigTest.java
+++ container/src/main/java/com/douzone/container/config/user/test/AppConfigTest.java
@@ -18,13 +18,12 @@ public class AppConfigTest {
 	// 직접 자바 클래스를 전달
 	// 설정 클래스에 @Configuration이 필요없다.
 	private static void testAppConfig01() {
+		ApplicationContext ac
+			= new AnnotationConfigApplicationContext(AppConfig01.class);
 		
-		// 컨테이너에 등록
-		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig01.class);
-		
-		User user = (User)ac.getBean(User.class);
+		User user = ac.getBean(User.class);
 		System.out.println(user);
-	
+		
 		((ConfigurableApplicationContext)ac).close();
 	}
 	
@@ -32,13 +31,12 @@ public class AppConfigTest {
 	// 자바 설정 클래스가 있는 베이스 패키지를 전달
 	// 설정 클래스에 @Configuration이 반드시 필요하다.
 	private static void testAppConfig02() {
+		ApplicationContext ac
+			= new AnnotationConfigApplicationContext("com.douzone.container.config.user");
 		
-		// 컨테이너에 등록, 직접 파일 전달하지 않고 패키지를 전달 - @Configuratin 가져다가 사용
-		ApplicationContext ac = new AnnotationConfigApplicationContext("com.douzone.container.config.user");
-		
-		User user = (User)ac.getBean(User.class);
+		User user = ac.getBean(User.class);
 		System.out.println(user);
-	
+		
 		((ConfigurableApplicationContext)ac).close();
 	}
 }
--- container/src/test/java/com/douzone/container/config/user/AppConfigTest.java
+++ container/src/test/java/com/douzone/container/config/user/AppConfigTest.java
@@ -16,21 +16,20 @@ public class AppConfigTest {
 	// 설정 클래스에 @Configuration이 필요없다.
 	@Test
 	public void testAppConfig01() {
-		
-		// 컨테이너에 등록
-		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig01.class);
-		User user = (User)ac.getBean(User.class);
-		assertNotNull(user); // 유저가 null이 아니다. null이면 빨간색 뜬다.
+		ApplicationContext ac
+			= new AnnotationConfigApplicationContext(AppConfig01.class);
+
+		User user = ac.getBean(User.class);
+		assertNotNull(user);
 	}
 	
 	// Java Config02
 	// 자바 설정 클래스가 있는 베이스 패키지를 전달
 	// 설정 클래스에 @Configuration이 반드시 필요하다.
 	@Test
-	private static void testAppConfig02() {
-		
-		// 컨테이너에 등록, 직접 파일 전달하지 않고 패키지를 전달 - @Configuratin 가져다가 사용
+	public void testAppConfig02() {
 		ApplicationContext ac = new AnnotationConfigApplicationContext("com.douzone.container.config.user");
-		User user = (User)ac.getBean(User.class);		
+		
+		User user = ac.getBean(User.class);
 	}
 }
